781b2c3a43c8dfc12f81f4a3edc477133f7f9faf
Kirkulese <kirksc91@gmail.com>
2021-06-01 12:59:29 -0700

    react basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar'
+import Useritem from './components/users/Useritem'
+
+class App extends Component {
+  foo = () => 'barrrr';
+
+  render(){
+
+
+    return (
+      <div className="App">
+       <Navbar title="Github Finder" icon='fab fa-github'/>
+       <Useritem />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
