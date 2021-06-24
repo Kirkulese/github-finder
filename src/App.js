4f183e7f9905434978d95fdba475742bb5e2a958
Kirkulese <kirksc91@gmail.com>
2021-06-24 08:38:45 -0700

    using router tag not working
    using router tag not working

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar'
 import Alert from './components/layout/Alert'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
 import axios from 'axios'
 import About from './components/pages/About'
@@ -13,6 +14,7 @@ class App extends Component {
 
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   }
@@ -30,6 +32,14 @@ class App extends Component {
 
     this.setState({ users: res.data.items, loading: false })
   }
+//find user detail
+  getUser = async (username) => {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+    this.setState({ user: res.data, loading: false })
+  }
+
 
   clearUsers = () => this.setState({ users: [], loading:false })
 
@@ -51,18 +61,24 @@ class App extends Component {
        <div className = "container">
          <Alert alert={this.state.alert} />
          <Switch>
-           <Route exact path='/' >
-              <Search /* give this a prop that we can use in the Search.js
+           <Route exact path='/' render={props => {
+             <Fragment>
+                <Search /* give this a prop that we can use in the Search.js
               but that prop calls a function from this file app.js to keep it
               centralized */
               searchUsers={this.searchUsers} clearUsers={this.clearUsers} 
               showClear={ this.state.users.length > 0 ? true: false }
               setAlert={this.setAlert} />
               <Users loading={this.state.loading} users={this.state.users} />
-           </Route>
+             </Fragment>
+           }} />
+           
            <Route exact path='/about'>
               <About />
            </Route>
+           <Route exact path='/user/:login' render={props => (
+              <User {...props} getUser={this.getUser} user={this.state.user} loading={this.state.loading} />
+           )}/>
          </Switch>
         
        </div>
