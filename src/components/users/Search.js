42ab8092afa735c70be687698d0bfc3236d69024
Kirkulese <kirksc91@gmail.com>
2021-07-19 14:04:59 -0700

    refactor not working currently. shows issue with getUser line under  useEffect

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,52 +1,44 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 
-export class Search extends Component {
-    state = {
-        text: ''
-    }
+const Search = ( { searchUsers, showClear, clearUsers, setAlert} ) => {
+    const [text, setText] = useState('')
+    
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired,
-    }
+    
     //need this function to be able to type in search since we are setting the search
     //value equal to state which is an object essentially
-    onChange = (e) => {
+     const onChange = (e) => {
         //used to be text:, [e.target.name] makes it more flexible, gets the input from
         //name which is = to "text" in the input html below
-        this.setState({ [e.target.name]: e.target.value })
+        setText(e.target.value)
     }
 
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault()
         //alert for empty searches
-        if(this.state.text === ''){
-            this.props.setAlert('Please input a search', 'light')
+        if(text === ''){
+            setAlert('Please input a search', 'light')
         } else {
             //this is where we call a prop function from APP.js and pass in 
             //search bar text
-            this.props.searchUsers(this.state.text)
-            this.setState({ text: '' })
+            searchUsers(text)
+            setText('')
         }
 
 
 
         
     }
-
-    render() {
-        const { showClear, clearUsers } = this.props
+        
 
         return (
             <div>
-                <form onSubmit={this.onSubmit} className="form">
+                <form onSubmit={onSubmit} className="form">
 
                     <input type="text" name="text" placeholder="Search Users..." 
-                    value={this.state.text} onChange={this.onChange}/>
+                    value={text} onChange={onChange}/>
 
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
@@ -54,7 +46,14 @@ export class Search extends Component {
                 
             </div>
         )
-    }
+    
+}
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
 }
 
 export default Search
